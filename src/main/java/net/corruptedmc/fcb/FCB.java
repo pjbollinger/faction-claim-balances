ba345d3371e08a730a69757411bc78b67675d693
Patrick Bollinger <pjbollinger@student.ysu.edu>
2014-04-21 10:17:41 -0400

    Setup basic listener.

--- src/main/java/net/corruptedmc/fcb/FCB.java
+++ src/main/java/net/corruptedmc/fcb/FCB.java
@@ -1,8 +1,28 @@
 package net.corruptedmc.fcb;
 
+import com.massivecraft.factions.event.LandClaimEvent;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class FCB extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onClaim(LandClaimEvent event) {
+
+        getLogger().info(event.getFaction().getTag() +
+                        " has " +
+                        event.getFaction().getPower() +
+                        " out of " +
+                        event.getFaction().getPowerMaxRounded() +
+                        " power."
+        );
+
+    }
 
 }
